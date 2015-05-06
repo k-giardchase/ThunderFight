dd436227bdf6e27dbbd429497d95d42ca7f2ad46
Kyle Giard-Chase & Virginie Trubiano & Bryan Borgeson & tommy bountham <student@epicodus.com>
2015-05-06 14:55:38 -0700

    Make animation for background work by deleting deprecated .css calls in the jQuery and replacing it with .x function calls

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,79 @@
-// GLOBALS
-var PLAYGROUND_HEIGHT = 250;
-var PLAYGROUND_WIDTH  = 700;
-var REFRESH_RATE      = 30;
+// Global constants:
+var PLAYGROUND_WIDTH     = 700;
+var PLAYGROUND_HEIGHT    = 250;
+var REFRESH_RATE         = 30;
 
-// Constants for gameplay
-var smallStarSpeed    = 1;
-var mediumStarSpeed   = 3;
-var bigStarSpeed      = 5;
+//Constants for the gameplay
+var smallStarSpeed        = 1; //pixels per frame
+
+var mediumStarSpeed       = 3; //pixels per frame
+
+var bigStarSpeed          = 5; //pixels per frame
 
 
 // jQuery
-$(document).ready(function() {
+$(function(){
 
-$("#playground").playground({ height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH })
-    .addGroup("background", { width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT }).end()
-    .addGroup("actors", { width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT }).end()
-    .addGroup("playerMissileLayer", { width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT}).end()
-    .addGroup("enemiesMissileLayer", { width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT });
+    // Initialize the game:
+    $("#playground").playground({ height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH });
 
-var background1 = new $.gameQuery.Animation({imageURL: "img/background1.png"});
-var background2 = new $.gameQuery.Animation({imageURL: "img/background2.png"});
-var background3 = new $.gameQuery.Animation({imageURL: "img/background3.png"});
-var background4 = new $.gameQuery.Animation({imageURL: "img/background4.png"});
-var background5 = new $.gameQuery.Animation({imageURL: "img/background5.png"});
-var background6 = new $.gameQuery.Animation({imageURL: "img/background6.png"});
+    // Animations declaration (background)
+    var background1 = new $.gQ.Animation({
+        imageURL: "http://gamequeryjs.com/demos/3/background1.png"});
+    var background2 = new $.gQ.Animation({
+        imageURL: "http://gamequeryjs.com/demos/3/background2.png"});
+    var background3 = new $.gQ.Animation({
+        imageURL: "http://gamequeryjs.com/demos/3/background3.png"});
+    var background4 = new $.gQ.Animation({
+        imageURL: "http://gamequeryjs.com/demos/3/background4.png"});
+    var background5 = new $.gQ.Animation({
+        imageURL: "http://gamequeryjs.com/demos/3/background5.png"});
+    var background6 = new $.gQ.Animation({
+        imageURL: "http://gamequeryjs.com/demos/3/background6.png"});
 
-    $("#background")
-        .addSprite("background1", {animation: background1,
-            width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
-        .addSprite("background2", {animation: background2,
-            width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT,
-            posx: PLAYGROUND_WIDTH})
-        .addSprite("background3", {animation: background3,
-            width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
-        .addSprite("background4", {animation: background4,
-            width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT,
-            posx: PLAYGROUND_WIDTH})
-        .addSprite("background5", {animation: background5,
-            width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
-        .addSprite("background6", {animation: background6,
-            width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT,
-            posx: PLAYGROUND_WIDTH});
 
-    $("#welcomeScreen").click(function() {
+    // Initialize the background
+    $.playground()
+        .addGroup("background", { width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT })
+        .addSprite("background1", { animation: background1, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT })
+        .addSprite("background2", { animation: background2, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT, posx: PLAYGROUND_WIDTH })
+        .addSprite("background3", { animation: background3, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT })
+        .addSprite("background4", { animation: background4, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT, posx: PLAYGROUND_WIDTH })
+        .addSprite("background5", { animation: background5, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT })
+        .addSprite("background6", { animation: background6, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT, posx: PLAYGROUND_WIDTH })
+
+
+    //initialize the start button
+    $("#startbutton").click(function() {
         $.playground().startGame(function() {
             $("#welcomeScreen").remove();
         });
-    })
+    });
 
+
+    //This is for the background animation
     $.playground().registerCallback(function(){
-         //offset all the pane:
-         var newPos = (parseInt($("#background1").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
-         %(-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-         $("#background").css("left", newPos);
 
-         newPos = (parseInt($("#background2").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
-         %(-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-         $("#background").css("left", newPos);
+        var newPos = ($("#background1").x() - smallStarSpeed - PLAYGROUND_WIDTH) % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+        $("#background1").x(newPos);
 
-         newPos = (parseInt($("#background3").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
-         %(-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-         $("#background").css("left", newPos);
+        newPos = ($("#background2").x() - smallStarSpeed - PLAYGROUND_WIDTH) % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+        $("#background2").x(newPos);
 
-         newPos = (parseInt($("#background4").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
-         %(-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-         $("#background").css("left", newPos);
+        newPos = ($("#background3").x() - mediumStarSpeed - PLAYGROUND_WIDTH) % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+        $("#background3").x(newPos);
 
-         newPos = (parseInt($("#background5").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
-         %(-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-         $("#background").css("left", newPos);
+        newPos = ($("#background4").x() - mediumStarSpeed - PLAYGROUND_WIDTH) % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+        $("#background4").x(newPos);
 
-         newPos = (parseInt($("#background6").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
-         %(-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-         $("#background").css("left", newPos);
+        newPos = ($("#background5").x() - bigStarSpeed - PLAYGROUND_WIDTH) % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+        $("#background5").x(newPos);
 
-     }, REFRESH_RATE);
+        newPos = ($("#background6").x() - bigStarSpeed - PLAYGROUND_WIDTH) % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+        $("#background6").x(newPos);
+    }, REFRESH_RATE);
 
 
 
 
-
-
-
-     })
+});  // CLOSING jQuery
